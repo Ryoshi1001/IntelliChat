92da3175a40754f9c2b3b591169376c216e5cd13
Me <ryoshi16421@proton.me>
2025-09-02 22:25:48 -0700

    feature(fix) remove unused Login component import in app.tsx file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,6 @@ import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
-import Login from "./pages/Login";
 
 const App = () => {
 const { authUser } = useContext(AuthContext)!; 
